27dde96ce68c93bb6be6faf6b25969e837a362e7
omeiza <omeiza.olumoye@gmail.com>
2024-09-24 11:41:53 -0400

    add a function that prints qualities on the screen after it loads

---
+++ js/qty.js
@@ -0,0 +1,18 @@
+
+let words = ['Lifelong Learner', 'Sport Supporter', 'Teacher at Heart'];
+
+// Get the output element by its ID
+const outputDiv = document.getElementById('quality');
+
+function printQualities() {
+  words.forEach((word, index) => {
+    setTimeout(() => {
+      if (outputDiv) {
+        outputDiv.innerHTML += `<p class="word">${word}</p>`;
+      }
+    }, (index+1) * 1000);  // 1000ms delay between each word
+  });
+}
+
+// function call
+printQualities();
